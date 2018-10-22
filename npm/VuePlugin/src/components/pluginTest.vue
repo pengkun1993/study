96583db0df18cb9bdf4034baf925588fd4e4a25e
pengkun <782041896@qq.com>
2018-10-22 16:24:36 +0800

    私服cnpm操作

---
+++ npm/VuePlugin/src/components/pluginTest.vue
@@ -0,0 +1,39 @@
+<template>
+	<div>
+		<div>test</div>
+		<p>
+			<button @click="getChildrenFunction">
+				点击触发子组件的toast
+			</button>
+		</p>
+		<div>
+			<p>点击面板上的内容</p>
+		</div>
+		<test-panel ref="panel"></test-panel>
+		<test-toast ref="toast"></test-toast>
+	</div>
+</template>
+<script>
+export default {
+	data(){
+		return {
+			testArr:[1,2,6]
+		}
+	},
+	components:{},
+	created(){
+		console.log('现在的api:',this.$msg);
+		console.log(this.$myMethod(this.testArr));
+		this.$nextTick(()=>{
+			console.log(this.$refs.panel.$el);
+		})
+	},
+	methods:{
+		getChildrenFunction(){
+			this.$nextTick(()=>{
+				this.$refs.toast.toastPlugin('在父组件里面触发调用的toast',2500);
+			})
+		}
+	}
+}
+</script>
--- npm/VuePlugin/src/main.js
+++ npm/VuePlugin/src/main.js
@@ -3,8 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import vueToastPanel from './plugin/test.js'
 
 Vue.config.productionTip = false
+
+Vue.use(vueToastPanel)
 
 /* eslint-disable no-new */
 new Vue({
